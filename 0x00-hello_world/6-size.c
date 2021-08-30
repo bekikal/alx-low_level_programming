7abcaf7220b70c24e1328c97ce2dddcf470d9608
Berhe Kalayu <bekikal2@gmail.com>
2021-08-30 11:18:18 -0700

    size of datatypes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *ksjdkfjlkjia
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int intType;
+	float floatType;
+	long long int llType;
+	char charType;
+	long int lType;
+
+	ptintf("Size of a char: %zu bytes (s\n)", Sizeof(charTyp));
+	ptintf("Size of a int: %zu bytes (s\n)", Sizeof(intType));
+        ptintf("Size of a long int: %zu bytes (s\n)", Sizeof(lType));
+        ptintf("Size of a long long int: %zu bytes (s\n)", Sizeof(llType));
+	ptintf("Size of a float: %zu bytes (s\n)", Sizeof(floatType));
+	
+        return (0);
+}
+	  
