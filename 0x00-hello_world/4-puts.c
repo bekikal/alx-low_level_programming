9b532fc573e68070f11e2ee2b0e47560e2d36616
Berhe Kalayu <bekikal2@gmail.com>
2021-08-30 10:38:07 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,7 @@
  */
 int main1(void)
 {
-	pusts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-
-		return (0);
-		}
+	return (0)
+ }
