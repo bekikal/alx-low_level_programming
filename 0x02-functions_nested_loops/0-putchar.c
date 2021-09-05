d5c4de4170097068ac7b5203be00e9d2f342c981
Berhe Kalayu <bekikal2@gmail.com>
2021-09-05 14:00:59 -0700

    prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,17 @@
+#include <stdio.h>
 #include "main.h"
-#include <unistd.h>
-
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ *This prints Holberton followed by new line.
+ *Return: it returns 0
  */
-int _putchar(char c)
+
+int main(void)
 {
-	return (write(1, &c, 1));
+        char c[8] = "_putchar";
+        int i;
+        for(i=0; i<8; i++){
+                _putchar(c[i]);
+        }
+        _putchar('\n');
+        return(0);
 }
