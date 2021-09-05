8343cb28b324e336a7f024f48bc5fe98a4454363
Berhe Kalayu <bekikal2@gmail.com>
2021-09-05 07:48:17 -0700

    adds two integers and returns the result.

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,16 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code for ALX School students.
+ * add - Adds two integers.
+ * @num1: The first integer to be added.
+ * @num2: The second integer to be added.
  *
- * Return: Always 0.
+ * Return: The result of the addition.
  */
-int main(void)
+int add(int num1, int num2)
 {
-    int n;
-
-    n = add(8, 12);
-    printf("%d\n", n);
-    return (0);
+	return (num1 + num2);
 }
