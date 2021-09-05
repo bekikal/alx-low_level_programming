120bb8c9e0f22a807b2c215cac20f7f5a458759f
Berhe Kalayu <bekikal2@gmail.com>
2021-09-05 07:00:15 -0700

    print 10x the alpshbet

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,13 +1,22 @@
 #include "main.h"
+
 /**
+ * main - Prints "Holberton" followed by a new line.
  *
- *main entry.
- *Return: 0.
+ * Return: Always 0.
  */
-
-
-int main(void )
+int main(void)
 {
-	print_alphabet();
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
 	return (0);
 }
