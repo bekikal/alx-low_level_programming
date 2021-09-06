b4eda8c624607ad13344820af3f60e74acac9ffa
Berhe Kalayu <bekikal2@gmail.com>
2021-09-06 12:25:30 -0700

    prints a string, followed by a new line, to stdout.

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,13 @@
 #include "main.h"
 
 /**
- * _strlen - Returns the length of a string.
- * @str: The string to get the length of.
- *
- * Return: The length of @str.
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
  */
-size_t _strlen(const char *str)
+void _puts(char *str)
 {
-	size_t length = 0;
+	while (*str)
+		_putchar(*str++);
 
-	while (*str++)
-		length++;
-
-	return (length);
+	_putchar('\n');
 }
