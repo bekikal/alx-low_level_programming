f9f27076605792b7c82c702ec9cab0b05a713f43
Berhe Kalayu <bekikal2@gmail.com>
2021-09-07 12:42:23 -0700

    prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
- *This prints Holberton followed by new line.
+ *main - call the _putchar() function.
+ *_putchar -  prints _putchart followed by new line.
+ *c : character to be char.
  *Return: it returns 0
  */
-
 int main(void)
 {
-        char c[8] = "_putchar";
-        int i;
-        for(i=0; i<8; i++){
-                _putchar(c[i]);
-        }
-        _putchar('\n');
-        return(0);
+	char c[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(c[i]);
+	_putchar('\n');
+	return (0);
 }
