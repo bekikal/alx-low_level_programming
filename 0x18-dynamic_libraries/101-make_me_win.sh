e4dffc196425366349c02c5705c5f57cba5319d6
Berhe Kalayu <bekikal2@gmail.com>
2021-12-06 00:24:38 -0800

    final

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /./unrandom.so https://github.com/bekikal/alx-low_level_programming/raw/master/0x17-dynamic_libraries/unrandom.so 
+wget -q -O /./unrandom.so https://github.com/bekikal/alx-low_level_programming/raw/master/0x18-dynamic_libraries/unrandom.so 
 export LD_PRELOAD=/./unrandom.so
